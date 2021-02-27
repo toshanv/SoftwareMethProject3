6b750ad9693b9312e05a2e0ca4071d7dcf6ca138
Christopher Nguyen <christophernguyen41@gmail.com>
2021-02-27 17:23:53 -0500

    {add} display to fxml file

--- src/payroll/Controller.java
+++ src/payroll/Controller.java
@@ -1,4 +1,21 @@
 package payroll;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
+    ObservableList <String> list = FXCollections.observableArrayList("Fulltime", "Parttime", "Management");
+    @FXML
+    private ChoiceBox series = new ChoiceBox();
+
+    @FXML
+    public void initialize () {
+        series.getItems().addAll(list);
+    }
+
 }
