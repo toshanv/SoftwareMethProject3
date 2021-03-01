34810a1f7ab3ce08e3ca4bffca41bceced0a23b8
Christopher Nguyen <christophernguyen41@gmail.com>
2021-02-28 23:57:00 -0500

    [ADD] Reset button functionality

--- src/payroll/Controller.java
+++ src/payroll/Controller.java
@@ -2,8 +2,12 @@ package payroll;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,10 +30,35 @@ public class Controller {
     private ChoiceBox managementStatus = new ChoiceBox();
 
     @FXML
+    private DatePicker hiredDate = new DatePicker();
+
+    @FXML
+    private TextField name = new TextField();
+
+    @FXML
+    private TextField annualSalary = new TextField();
+
+    @FXML
+    private TextField hoursWorked = new TextField();
+
+    @FXML
+    private TextField hourlyRate = new TextField();
+
+    @FXML
     public void initialize () {
         employmentStatus.getItems().addAll(employmentStatusList);
         departmentStatus.getItems().addAll(departmentList);
         managementStatus.getItems().addAll(managementList);
     }
 
+    public void resetTab(ActionEvent actionEvent) {
+        name.clear();
+        annualSalary.clear();
+        hoursWorked.clear();
+        hourlyRate.clear();
+        departmentStatus.setValue(null);
+        employmentStatus.setValue(null);
+        managementStatus.setValue(null);
+        hiredDate.setValue(null);
+    }
 }
