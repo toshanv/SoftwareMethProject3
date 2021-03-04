985499419538cd7a8b09d9f93b3c9543727ebebe
Toshan Vysyaraju <toshanv2000@gmail.com>
2021-03-03 21:36:03 -0500

    [ ADD ] handleClickAdd in Controller.java

--- src/payroll/Controller.java
+++ src/payroll/Controller.java
@@ -21,6 +21,14 @@ public class Controller {
 
     ObservableList <String> managementList = FXCollections.observableArrayList("Manager", "DepartmentHead", "Director");
 
+    Company company = new Company();
+
+    private static final int PARTTIME_CODE = 1;
+    private static final int FULLTIME_CODE = 2;
+    private static final int MANAGEMENT_CODE = 3;
+
+    private static final int ERROR_CODE = -1;
+
 
     @FXML
     private ChoiceBox employmentStatus = new ChoiceBox();
@@ -46,6 +54,8 @@ public class Controller {
     @FXML
     private TextField hourlyRate = new TextField();
 
+    @FXML
+    private TextArea textDisplay = new TextArea();
 
     @FXML
     public void initialize () {
@@ -71,22 +81,17 @@ public class Controller {
             hoursWorked.setDisable(false);
             hourlyRate.setDisable(false);
             managementStatus.setDisable(true);
-        }
-        else if (employmentStatus.getValue() == "Fulltime" ) {
+        } else if (employmentStatus.getValue() == "Fulltime" ) {
             annualSalary.setDisable(false);
             hoursWorked.setDisable(true);
             hourlyRate.setDisable(true);
             managementStatus.setDisable(true);
-
-
-        }
-        else if (employmentStatus.getValue() == "Management") {
+        } else if (employmentStatus.getValue() == "Management") {
             annualSalary.setDisable(false);
             hoursWorked.setDisable(true);
             hourlyRate.setDisable(true);
             managementStatus.setDisable(false);
-        }
-        else {
+        } else {
             annualSalary.setDisable(false);
             hoursWorked.setDisable(false);
             hourlyRate.setDisable(false);
@@ -97,44 +102,98 @@ public class Controller {
     public int checkFieldDouble () {
         try {
             Double checkVal;
-            if (employmentStatus.getValue() == "Parttime"){
+            if (employmentStatus.getValue() == "Parttime") {
                 checkVal = Double.parseDouble(hourlyRate.getText());
-                checkVal = Double.parseDouble(hoursWorked.getText());
-                return 1;
-            }
-            else if (employmentStatus.getValue() == "Management" || employmentStatus.getValue() == "Fulltime") {
+                // checkVal = Double.parseDouble(hoursWorked.getText());
+                return PARTTIME_CODE;
+            } else if (employmentStatus.getValue() == "Management" || employmentStatus.getValue() == "Fulltime") {
                 checkVal = Double.parseDouble(annualSalary.getText());
                 if (employmentStatus.getValue() == "Management") {
-                    return 3;
+                    return MANAGEMENT_CODE;
+                } else {
+                    return FULLTIME_CODE;
                 }
-                else{
-                    return 2;
-                }
-            }
-            else{
+            } else {
                 Alert errorAlert = new Alert(Alert.AlertType.ERROR);
                 errorAlert.setHeaderText("Submission is not valid");
                 errorAlert.setContentText("One of the required fields is empty or is not a valid type double.");
                 errorAlert.showAndWait();
-                return -1;
+                return ERROR_CODE;
             }
-        }
-        catch (NumberFormatException ex) {
+        } catch (NumberFormatException ex) {
             Alert errorAlert = new Alert(Alert.AlertType.ERROR);
             errorAlert.setHeaderText("Submission is not valid");
             errorAlert.setContentText("One of the required fields is empty or is not a valid type double.");
             errorAlert.showAndWait();
-            return -1;
+            return ERROR_CODE;
         }
     }
 
     public void handleClickAdd(ActionEvent actionEvent) {
-       int check = checkFieldDouble();
-       if (check == -1) {
+        final int MANAGER_CODE = 1;
+        final int DEPT_HEAD_CODE = 2;
+        final int DIRECTOR_CODE = 3;
+
+        int employeeType = checkFieldDouble();
+        if (employeeType == ERROR_CODE) {
            resetTab(actionEvent);
            return;
-       }
-       System.out.println(check);
+        }
+
+        String inputDate = hiredDate.getValue().toString();
+        inputDate = inputDate.substring(5, 7) + "/" + inputDate.substring(8, 10) + "/" + inputDate.substring(0, 4);
+        Date dateHired = new Date(inputDate);
+
+        if (!dateHired.isValid()) {
+            textDisplay.appendText(inputDate + " is not a valid date!\n");
+            resetTab(actionEvent);
+            return;
+        }
+
+        Profile profile = new Profile(name.getText(), departmentStatus.getValue().toString(), dateHired);
+
+        Employee employee = null;
+        if (employeeType == PARTTIME_CODE) {
+            double inputRate = Double.parseDouble(hourlyRate.getText());
+
+            if (inputRate < 0) {
+                textDisplay.appendText("Pay rate cannot be negative.\n");
+                resetTab(actionEvent);
+                return;
+            }
+
+            employee = new Parttime(profile, 0, inputRate, 0);
+        } else {
+            double inputPay = Double.parseDouble(annualSalary.getText());
+
+            if (inputPay < 0) {
+                textDisplay.appendText("Salary cannot be negative.\n");
+                resetTab(actionEvent);
+                return;
+            }
+
+            if (employeeType == FULLTIME_CODE) {
+                employee = new Fulltime(profile, 0, inputPay);
+            } else {
+                int managementCode;
+                if (managementStatus.getValue().toString().equals("Manager")) {
+                    managementCode = MANAGER_CODE;
+                } else if (managementStatus.getValue().toString().equals("DepartmentHead")) {
+                    managementCode = DEPT_HEAD_CODE;
+                } else {
+                    managementCode = DIRECTOR_CODE;
+                }
+
+                employee = new Management(profile, 0, inputPay, managementCode);
+            }
+        }
+
+        boolean addResult = company.add(employee);
+        if (!addResult) {
+            textDisplay.appendText("Employee is already in the list.\n");
+        } else {
+            textDisplay.appendText("Employee added.\n");
+        }
     }
 
 }
