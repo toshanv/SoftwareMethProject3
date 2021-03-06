ef822832ce3dd93a0002018c726fab6c9de8f6fc
Christopher Nguyen <christophernguyen41@gmail.com>
2021-03-05 23:31:01 -0500

    [ADD] added function handleClickSetHours

--- src/payroll/Controller.java
+++ src/payroll/Controller.java
@@ -146,7 +146,7 @@ public class Controller {
         if (name.getText().equals("") || departmentStatus.getValue() == null || hiredDate.getValue() == null) {
             Alert errorAlert = new Alert(Alert.AlertType.ERROR);
             errorAlert.setHeaderText("Submission is not valid");
-            errorAlert.setContentText("One of the required fields is empty or is not a valid type double.");
+            errorAlert.setContentText("One of the required fields is empty.");
             errorAlert.showAndWait();
             resetTab(actionEvent);
             return;
@@ -201,9 +201,8 @@ public class Controller {
                 if (managementStatus.getValue() == null) {
                     Alert errorAlert = new Alert(Alert.AlertType.ERROR);
                     errorAlert.setHeaderText("Submission is not valid");
-                    errorAlert.setContentText("One of the required fields is empty or is not a valid type double.");
+                    errorAlert.setContentText("Management Roll is empty please fill out.");
                     errorAlert.showAndWait();
-                    resetTab(actionEvent);
                     return;
                 }
 
@@ -225,6 +224,7 @@ public class Controller {
         } else {
             textDisplay.appendText("Employee added.\n");
         }
+        resetTab(actionEvent);
     }
 
     public void handleClickPrint(ActionEvent actionEvent) {
@@ -243,7 +243,7 @@ public class Controller {
         if (name.getText().equals("") || departmentStatus.getValue() == null || hiredDate.getValue() == null) {
             Alert errorAlert = new Alert(Alert.AlertType.ERROR);
             errorAlert.setHeaderText("Submission is not valid");
-            errorAlert.setContentText("One of the required fields is empty or is not a valid type double.");
+            errorAlert.setContentText("One of the required fields is empty.");
             errorAlert.showAndWait();
             resetTab(actionEvent);
             return;
@@ -277,6 +277,82 @@ public class Controller {
             }
         }
 
+        resetTab(actionEvent);
+
     }
 
+    public void handleClickSetHours(ActionEvent actionEvent) {
+        int inputHours;
+
+        if (name.getText().equals("") || departmentStatus.getValue() == null || hiredDate.getValue() == null || hoursWorked.getText().equals("")) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Submission is not valid");
+            errorAlert.setContentText("One of the required fields is empty.");
+            errorAlert.showAndWait();
+            resetTab(actionEvent);
+            return;
+        }
+        if (!employmentStatus.getValue().toString().equals("Parttime")) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Submission is not valid");
+            errorAlert.setContentText("Cannot Set Hours for Management or Fulltime only Parttime");
+            errorAlert.showAndWait();
+            return;
+        }
+
+        try {
+            if (hoursWorked.getText() != "") {
+                inputHours = Integer.parseInt(hoursWorked.getText());
+            } else {
+                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setHeaderText("Submission is not valid");
+                errorAlert.setContentText("Hours Worked is empty fill in.");
+                errorAlert.showAndWait();
+                return;
+            }
+        } catch (NumberFormatException ex) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Submission is not valid");
+            errorAlert.setContentText("Hours work is not a valid type int.");
+            errorAlert.showAndWait();
+            return;
+        }
+        if (company.getNumEmployee() == 0) {
+            textDisplay.appendText("Employee database is empty.\n");
+            return;
+        }
+
+        String inputDate = hiredDate.getValue().toString();
+        inputDate = inputDate.substring(5, 7) + "/" + inputDate.substring(8, 10) + "/" + inputDate.substring(0, 4);
+        Date dateHired = new Date(inputDate);
+
+        if (!dateHired.isValid()) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Submission is not valid");
+            errorAlert.setContentText("Date is not a valid date. Try again.");
+            errorAlert.showAndWait();
+            resetTab(actionEvent);
+            return;
+        }
+
+        if (inputHours > 100) {
+            textDisplay.appendText("Invalid Hours: over 100.\n");
+            return;
+        } else if (inputHours < 0) {
+            textDisplay.appendText("Working hours cannot be negative.\n");
+            return;
+        }
+
+
+        Profile profile = new Profile(name.getText(), departmentStatus.getValue().toString(), dateHired);
+        Employee employeeToSetHours = new Parttime(profile, 0, 0, inputHours);
+        Boolean isUpdated = company.setHours(employeeToSetHours);
+
+        if (isUpdated) {
+            textDisplay.appendText("Working hours set.\n");
+        } else {
+            textDisplay.appendText("Employee does not exist.\n");
+        }
+        resetTab(actionEvent);
+    }
 }
